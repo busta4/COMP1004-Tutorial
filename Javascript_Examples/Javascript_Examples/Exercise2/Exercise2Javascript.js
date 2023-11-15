3452a6751ddd2087bf5e98bbad7c19f5d59f9d43
Bustamante Barrett-Greening <146181717+busta4@users.noreply.github.com>
2023-11-15 10:05:07 +0000

    added task files

---
+++ Javascript_Examples/Javascript_Examples/Exercise2/Exercise2Javascript.js
@@ -0,0 +1,23 @@
+
+function MakeTextSmaller()
+{
+	document.getElementByid("textSize").style = "px 50";
+}
+
+
+function ReplaceText()
+{
+	document.getElementById("placeholderText").innerHTML = "My new text!";
+}
+
+
+
+function TurnLightOn()
+{
+
+}
+
+function TurnLightOff()
+{
+	
+}
